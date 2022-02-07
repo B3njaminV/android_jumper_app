58eca53a0b9ba0a2927e32cd97f215cbeecf1402
Benjamin <Benjamin.valleix@gmail.com>
2022-02-07 22:54:21 +0100

    ajout de la persistence légère et du popup pseudo invalide en cas de pseudo vide

--- app/src/main/java/app/android_jumper_app/FenetreDeJeu.java
+++ app/src/main/java/app/android_jumper_app/FenetreDeJeu.java
@@ -1,63 +1,44 @@
 package app.android_jumper_app;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 public class FenetreDeJeu extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fenetredejeu);
+        Log.d("LAJ","FJ-onCreate");
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        //String nom = ((TextView)findViewById(R.id.textView)).setText());
-
-        Log.d("LOGAPPJUMPER","onStart");
+        ((TextView)findViewById(R.id.textView)).setText("Salut " + getIntent().getStringExtra("joueur"));
+        Log.d("LAJ","FJ-onStart");
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        Log.d("LOGAPPJUMPER","onResume");
+        Log.d("LAJ","FJ-onResume");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Log.d("LOGAPPJUMPER","onPause");
+        Log.d("LAJ","FJ-onPause");
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Log.d("LOGAPPJUMPER","onDestroy");
+        Log.d("LAJ","FJ-onDestroy");
     }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState){
-        Log.d("LOGAPPJUMPER","ONSAVEINSTANCESTATE");
-        //outState.putString("joueur", joueur.getPseudo());
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle saveInstanceState){
-        Log.d("LOGAPPJUMPER","ONRESTOREINSTANCESTATE");
-        //joueur.setPseudo(saveInstanceState.getString("joueur"));
-        super.onRestoreInstanceState(saveInstanceState);
-    }
-
 }
--- app/src/main/java/app/android_jumper_app/FenetrePrincipale.java
+++ app/src/main/java/app/android_jumper_app/FenetrePrincipale.java
@@ -1,18 +1,14 @@
 package app.android_jumper_app;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-
+import com.google.android.material.snackbar.Snackbar;
 public class FenetrePrincipale extends AppCompatActivity {
 
     public Joueur joueur;
@@ -21,58 +17,64 @@ public class FenetrePrincipale extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fenetreprincipale);
-        joueur = new Joueur();
         if(savedInstanceState != null){
-                ((EditText)findViewById(R.id.editTextTextPersonName)).setText(savedInstanceState.getString("joueur"));
-                Log.d("LOGAPPJUMPER","JOUEUR BIEN RECUPERE");
+            ((EditText)findViewById(R.id.editTextTextPersonName)).setText(savedInstanceState.getString("joueur"));
+
         }
+        Log.d("LAJ","FP-onCreate");
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState){
-        Log.d("LOGAPPJUMPER","ONSAVEINSTANCESTATE");
+        Log.d("LAJ","FP-ONSAVEINSTANCESTATE");
         outState.putString("joueur", joueur.getPseudo());
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle saveInstanceState){
-        Log.d("LOGAPPJUMPER","ONRESTOREINSTANCESTATE");
+        Log.d("LAJ","FJ-ONRESTOREINSTANCESTATE");
         joueur.setPseudo(saveInstanceState.getString("joueur"));
         super.onRestoreInstanceState(saveInstanceState);
     }
 
-
     @Override
     protected void onStart(){
         super.onStart();
-        Log.d("LOGAPPJUMPER","onStart");
+        joueur = new Joueur();
+        Log.d("LAJ","FP-onStart");
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        Log.d("LOGAPPJUMPER","onResume");
+        Log.d("LAJ","FP-onResume");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Log.d("LOGAPPJUMPER","onPause");
+        Log.d("LAJ","FP-onPause");
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Log.d("LOGAPPJUMPER","onDestroy");
+        Log.d("LAJ","FP-onDestroy");
     }
 
     public void click(View view) {
         String nom = ((EditText)findViewById(R.id.editTextTextPersonName)).getText().toString();
-        joueur.setPseudo(nom);
-        Log.d("LOGAPPJUMPER","JOUEUR CREE");
-        Intent intent = new Intent(this, FenetreDeJeu.class);
-        startActivity(intent);
+        if (nom.isEmpty()){
+            Snackbar mySnackbar = Snackbar.make(view, "Pseudo Invalide !", 2000);
+            mySnackbar.show();
+        }else{
+            joueur.setPseudo(nom);
+            Log.d("LAJ","FP-JOUEUR CREE");
+            Intent intent = new Intent(this, FenetreDeJeu.class);
+            intent.putExtra("joueur", joueur.getPseudo());
+            startActivity(intent);
+        }
     }
 
 
